b286c4684f000ab59f1ec2afe45616c2e7f3a840
Phil Pell <134700971+philpell@users.noreply.github.com>
2023-08-10 14:34:48 +0100

    Slight change to text

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function changeX()  {
     }
       else dropdownBtn.textContent = "≡";
   }
+
+  function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+    
+    document.querySelector('.theme-name').textContent = newTheme;
+  }
+  
+  document.querySelector('.theme-toggle').addEventListener('click', setTheme)
